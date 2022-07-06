96fb5ec6f21a1132eb4b2b084ffe793669c378ca
Kapil Kumar <kr_kapil@hotmail.com>
2022-07-06 11:33:22 +0530

    apollo client setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:8200/graphql',
+  cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
